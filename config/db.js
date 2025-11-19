821092a7bd412e56664095149c1b921d0d5ba830
Thiago Yukio <leonardo_narcizo@estudante.sesisenai.org.br>
2025-11-19 09:47:31 -0300

    diminuindo o pooler

--- config/db.js
+++ config/db.js
@@ -30,7 +30,7 @@ const sequelize = new Sequelize(
   {
     dialect: 'postgres',
     logging: false,
-    pool: { min: 2, max: 10, idle: 10000, acquire: 30000 },
+    pool: { min: 2, max: 4, idle: 10000, acquire: 30000 },
     port: Number(process.env.DB_PORT) || 5432, // ⚠️ use 5432 como padrão
     dialectOptions: {
       ssl: {
