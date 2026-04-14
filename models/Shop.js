affdf45384f3a8e7e4009813a924a530120db669
LeonardoDreherN <leonardo_narcizo@estudante.sesisenai.org.br>
2026-04-14 14:56:08 -0300

    tabble shop

--- models/Shop.js
+++ models/Shop.js
@@ -2,28 +2,11 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
     const Shop = sequelize.define('Shop', {
-        shop: {
-            type: DataTypes.STRING,
-            primaryKey: true,
-            unique: true,
-        },
-        accessToken: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        refreshToken: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
-        tokenExpiresAt: {
-            type: DataTypes.DATE,
-            allowNull: true,
-        },
-        scope: {
-            type: DataTypes.TEXT,
-            allowNull: true,
-        },
+        shop: { type: DataTypes.STRING, primaryKey: true, unique: true },
+        accessToken: { type: DataTypes.TEXT, allowNull: false },
+        refreshToken: { type: DataTypes.TEXT }, // 👈 NOVO
+        tokenExpiresAt: { type: DataTypes.DATE }, // 👈 NOVO
+        scope: { type: DataTypes.TEXT },
     });
-
     return Shop;
 };
